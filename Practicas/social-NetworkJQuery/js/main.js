31ef0045cd934e50e5aadd0cd4494101a39964f2
Angel Valle <angelvalle@angel.local>
2021-04-15 17:02:42 -0500

    adding stuffs

--- Practicas/social-NetworkJQuery/js/main.js
+++ Practicas/social-NetworkJQuery/js/main.js
@@ -5,13 +5,37 @@ const hideReplies = () => {
   let firstChildGroup = $(".rw li:first-child");
   let allLi = $(".rw li");
   allLi.hide();
-
   firstChildGroup.each(function () {
     $(this).show();
   });
 };
 */
 
+//--------------- NUEVO ---------------
+
+const showComments = (event) => {
+  let idToSeeMore = event.target.dataset.postIdToComment;
+  repliesWrapper.each(function () {
+    this.dataset.postId === idToSeeMore
+      ? $(this).children().show()
+      : $(this).children().hide() && $(this).children(":first").show();
+  });
+};
+
+const deleteReply = (key) => {
+  $.ajax({
+    method: "DELETE",
+    url: `https://ajaxclass-1ca34.firebaseio.com/11g/drinktim/posts/${key}.json`,
+    success: (response) => {
+      console.log(response);
+    },
+    error: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+//----------- FIN NUEVO ------------
 let defaultUser = { name: "Israel", id: 007 };
 
 let postWrapper = $("#post-wrapper");
@@ -71,7 +95,6 @@ const getData = () => {
   return postsCollection;
 };
 
-
 const getReplies = () => {
   let repliesCollection;
 
@@ -88,7 +111,6 @@ const getReplies = () => {
   });
   return repliesCollection;
 };
-
 
 const addComment = (event) => {
   let id = event.target.dataset.postId;
@@ -119,8 +141,6 @@ const addComment = (event) => {
 };
 
 const printReplies = (repliesCollection) => {
-
-
   repliesWrapper.children().remove();
 
   for (key in repliesCollection) {
@@ -129,21 +149,21 @@ const printReplies = (repliesCollection) => {
     ];
 
     let reply = `
-            <li class="list-group-item">
-                <div class="reply-box">
-                  <div class="d-flex align-items-center">
-                    <img class="rounded-circle mr-2" style="height: 40px; width: 40px;" src="img/choppa.png" alt="user-img">
-                    <h3 class="font-weight-bold"><span>${author}</span></h3>
-                  </div>
-                    <p class="mt-3 font-italic" >${content}</p>
-                    <small class="text-right text-muted font-italic d-flex justify-content-end">
-                        <span class="date">${date}</span> 
-                        -
-                        <span class="time">${time}</span>   
-                    </small>
+          <li class="list-group-item">
+              <div class="reply-box">
+                <div class="d-flex align-items-center">
+                  <img class="rounded-circle mr-2" style="height: 40px; width: 40px;" src="img/choppa.png" alt="user-img">
+                  <h3 class="font-weight-bold"><span>${author}</span></h3>
                 </div>
-            </li>
-    `;
+                  <p class="mt-3 font-italic" >${content}</p>
+                  <small class="text-right text-muted font-italic d-flex justify-content-end">
+                      <span class="date">${date}</span> 
+                      -
+                      <span class="time">${time}</span>   
+                  </small>
+              </div>
+          </li>
+  `;
 
     repliesWrapper.each(function () {
       if (this.dataset.postId == postId) {
@@ -159,39 +179,38 @@ const printPost = (data) => {
     const { author, authorId, content, date, idPost, title, url } = data[key];
 
     let post = `
-    <div class="col-12">
-        <div id=${idPost} class="card shadow-sm rounded mb-3">
-           <div class="row no-gutters p-4">
-             <div class="col-md-4">
-               <img class="w-100" style="height: 200px;" src=${url} alt="img">
+  <div class="col-12">
+      <div id=${idPost} class="card shadow-sm rounded mb-3">
+         <div class="row no-gutters p-4">
+           <div class="col-md-4">
+             <img class="w-100" style="height: 200px;" src=${url} alt="img">
+           </div>
+           <div class="col-md-8 p-3">
+             <div "class="card-body ">
+               <h3 class="card-title font-weight-bold">${title}</h3>
+               <p class="text-muted font-italic"> ${author}</p>
+               <p class="card-text">${content}</p>
+               <p class="card-text d-flex justify-content-end"><small class="text-muted">Created <span class="text-dark">${date}</span></small></p>
              </div>
-             <div class="col-md-8 p-3">
-               <div "class="card-body ">
-                 <h3 class="card-title font-weight-bold">${title}</h3>
-                 <p class="text-muted font-italic"> ${author}</p>
-                 <p class="card-text">${content}</p>
-                 <p class="card-text d-flex justify-content-end"><small class="text-muted">Created <span class="text-dark">${date}</span></small></p>
-               </div>
-             </div>
            </div>
-
-           <div data-post-id=${idPost} class="replies-wrapper">
-           <div data-post-id=${idPost}  class="rw">
-   
-          
-           </div>
-           <div class="reply-form p-3 mt-2">
-               <form>
-                   <div class="form-group d-flex">
-                       <input data-reply-id=${idPost} name="reply" type="text" class="form-control" placeholder="Write a comment">
-                       <button data-post-id=${idPost} type="button" class="btn btn-success btn-comment">Comment</button>
-                   </div>
-               </form>
-           </div>
-       </div>
-       </div>
-   </div>
-           `;
+         </div>
+         <div data-post-id=${idPost} class="replies-wrapper">
+         <div data-post-id=${idPost}  class="rw">
+ 
+        
+         </div>
+         <div class="reply-form p-3 mt-2">
+             <form>
+                 <div class="form-group d-flex">
+                     <input data-reply-id=${idPost} name="reply" type="text" class="form-control" placeholder="Write a comment">
+                     <button data-post-id=${idPost} type="button" class="btn btn-success btn-comment">Comment</button>
+                 </div>
+             </form>
+         </div>
+     </div>
+     </div>
+ </div>
+         `;
 
     postWrapper.append(post);
   }
@@ -232,4 +251,15 @@ const savePost = (newPost) => {
 
 printReplies(getReplies());
 
+//----------- NUEVO -----------
+repliesWrapper.children().hide();
+repliesWrapper.each(function () {
+  let btnShow = `<button data-post-id-to-comment = ${this.dataset.postId} class="btn text-muted btn-see-more">See more comments</button>`;
+  $(this).children(":first").show();
+  $(this).children().length !== 1
+    ? $(this).children(":first").append(btnShow)
+    : null;
+});
+$(".btn-see-more").click(showComments);
 
+//--------- FIN NUEVO -------
